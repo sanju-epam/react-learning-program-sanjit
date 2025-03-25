b3a0ae56fed7d13dd2ce894ec8f49d5750f4caa5
sanju-epam <sanjit_kumar@epam.com>
2025-03-25 19:05:22 +0530

    fixed minor bug

--- mylearning-app/src/components/SearchForm.js
+++ mylearning-app/src/components/SearchForm.js
@@ -3,11 +3,11 @@ import React from 'react';
 class SearchBox extends React.Component {
     constructor(props) {
         super(props);
-        this.inputRef = React.createRef();
+        this.input = null;
     }
 
     handleSearch = () => {
-        const inputValue = this.inputRef.current.value;
+        const inputValue = this.input.value;
         if (this.props.onSearch) {
             this.props.onSearch(inputValue);
         }
@@ -18,13 +18,16 @@ class SearchBox extends React.Component {
             this.handleSearch();
         }
     };
-
+    setInput = (element) => {
+       
+        this.input = element;
+    };
     render() {
         return React.createElement(
             'div',
             { style: { display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: '20px' } },
             React.createElement('input', {
-                ref: this.inputRef,
+                ref: this.setInput,
                 type: 'text',
                 defaultValue: this.props.initialQuery || "",
                 onKeyPress: this.handleKeyPress,
