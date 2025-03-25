2495d54ada83e66d338fd31e3acaf6aa5c262b4e
sanju-epam <sanjit_kumar@epam.com>
2025-03-25 18:55:18 +0530

    Implemente module1 1. Counter component 2. SearchForm Components 3. GenereSelect component

---
+++ mylearning-app/src/components/GenreSelect.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class MovieGenres extends React.Component {
+  constructor(props) {
+    super(props);
+    this.selectedGenre = props.selectedGenre || ""; 
+  }
+  handleGenreSelect = (genre) => {
+    this.selectedGenre = genre; 
+    console.log(`Selected genre: ${genre}`);
+    if (this.props.onSelect) {
+      this.props.onSelect(genre); 
+    }
+    this.forceUpdate();
+  };
+
+  render() {
+    const { genres } = this.props; 
+
+    return (
+        <div  style={{ display: "flex", gap: "10px" }}>
+        {genres.map((genre) => (
+          <button
+            key={genre}
+            style={{
+              padding: "10px 15px",
+              backgroundColor: this.selectedGenre === genre ? "#007bff" : "#f0f0f0",
+              color: this.selectedGenre === genre ? "#fff" : "#000",
+              fontWeight: this.selectedGenre === genre ? "bold" : "normal",
+            }}
+            onClick={() => this.handleGenreSelect(genre)}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+    );
+  };
+}
+
+export default MovieGenres;
--- mylearning-app/src/index.js
+++ mylearning-app/src/index.js
@@ -1,15 +1,54 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Counter from './components/Counter';
+import SearchBox from './components/SearchForm';
 import reportWebVitals from './reportWebVitals';
+import MovieGenres from './components/GenreSelect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+let counterValue = 10;
+function updateCounter(newValue) {
+  counterValue = newValue;
+  render();
+}
+
+
+function handleSearch(query) {
+  console.log("Search triggered with query:", query);
+}
+
+let selectedGenre = "ALL"; 
+const genresList = ["ALL","DOCUMENTARY", "COMEDY", "HORROR", "CRIME"]; 
+function handleGenreSelect(newGenre) {
+  selectedGenre = newGenre; 
+  console.log("New selected movie:", newGenre);
+  render();
+}
+
+function render() {
+  root.render(
+      React.createElement(
+          React.StrictMode,
+          null,
+          React.createElement(
+              'div',
+              { style: { display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', marginTop: '50px' } },
+              React.createElement(SearchBox, { initialQuery: "Hello", onSearch: handleSearch }),
+              React.createElement(Counter, { initialValue: counterValue, onUpdate: updateCounter }),
+              React.createElement(MovieGenres, {
+                genres: genresList,
+                selectedGenre: selectedGenre, 
+                onSelect: handleGenreSelect,
+              })
+          )
+      )
+  );
+}
+
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
